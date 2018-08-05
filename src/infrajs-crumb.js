33ffc71381fe5749e9ede287caf63da09a1ae95f
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-08-05 23:22:21 +0400

    fix

--- src/infrajs-crumb.js
+++ src/infrajs-crumb.js
@@ -5,6 +5,6 @@ infrajs.setCrumb=function(layer,name,value){
 	var root=layer.parent?layer.parent[name]:infra.Crumb.getInstance();//От родителя всегда сможем наследовать
 
 	
-	if(layer.dyn[name])layer[name]=root.getInstance([layer.dyn[name]]);
+	if(layer.dyn[name])layer[name]=root.getInstance(layer.dyn[name]);
 	else layer[name]=root;
 }
